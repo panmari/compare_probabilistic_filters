9ee7e60abb7aeac1947ee84c893c80516b9db522
panmari <smmuzi@gmail.com>
2020-10-19 11:01:10 +0200

    Adding new benchmark with more realistic contain calls.
    In order to study effect of branch prediction on benchmarks.

--- benchmark_test.go
+++ benchmark_test.go
@@ -6,6 +6,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"testing"
 
@@ -17,8 +18,9 @@ import (
 )
 
 var (
-	words      []string
-	otherWords []string
+	words      []string // Words contained in filter.
+	otherWords []string // Words NOT contained in filter.
+	mixedWords []string // Mix of words that are contained/not contained.
 	numWords   = 500
 )
 
@@ -37,6 +39,18 @@ func init() {
 	for i := 0; i < maxNumWords && scanner.Scan(); i++ {
 		otherWords = append(otherWords, scanner.Text())
 	}
+	r := rand.New(rand.NewSource(0))
+	wordsIndex := 0
+	otherWordsIndex := 0
+	for i := 0; i < maxNumWords; i++ {
+		if r.Intn(2) == 0 {
+			mixedWords = append(mixedWords, words[wordsIndex])
+			wordsIndex++
+			continue
+		}
+		mixedWords = append(mixedWords, otherWords[otherWordsIndex])
+		otherWordsIndex++
+	}
 }
 
 func BenchmarkFilters(b *testing.B) {
@@ -49,6 +63,7 @@ func BenchmarkFilters(b *testing.B) {
 			b.Run("Insert", insert)
 			b.Run("ContainsTrue", containsTrue)
 			b.Run("ContainsFalse", containsFalse)
+			b.Run("containsMixed", containsMixed)
 		})
 	}
 }
@@ -230,6 +245,74 @@ func containsFalse(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; {
 			for _, w := range otherWords[:numWords] {
+				f.Lookup([]byte(w))
+			}
+			i += numWords
+		}
+	})
+}
+
+func containsMixed(b *testing.B) {
+	b.Run("Bloomfilter", func(b *testing.B) {
+		f, _ := bloomfilter.NewOptimal(uint64(numWords), 0.0001)
+		for _, w := range words[:numWords] {
+			f.Add(bloomHash(w))
+		}
+		b.ResetTimer()
+		for i := 0; i < b.N; {
+			for _, w := range mixedWords[:numWords] {
+				f.Contains(bloomHash(w))
+			}
+			i += numWords
+		}
+	})
+	b.Run("BBloom", func(b *testing.B) {
+		f := bbloom.New(float64(numWords), 0.002)
+		for _, w := range words[:numWords] {
+			f.Add([]byte(w))
+		}
+		b.ResetTimer()
+		for i := 0; i < b.N; {
+			for _, w := range mixedWords[:numWords] {
+				f.Has([]byte(w))
+			}
+			i += numWords
+		}
+	})
+	b.Run("SeiflotfyCuckoo", func(b *testing.B) {
+		f := cuckoo.NewFilter(uint(numWords))
+		for _, w := range words[:numWords] {
+			f.Insert([]byte(w))
+		}
+		b.ResetTimer()
+		for i := 0; i < b.N; {
+			for _, w := range mixedWords[:numWords] {
+				f.Lookup([]byte(w))
+			}
+			i += numWords
+		}
+	})
+	b.Run("PanmariCuckoo", func(b *testing.B) {
+		f := cuckooV2.NewFilter(uint(numWords))
+		for _, w := range words[:numWords] {
+			f.Insert([]byte(w))
+		}
+		b.ResetTimer()
+		for i := 0; i < b.N; {
+			for _, w := range mixedWords[:numWords] {
+				f.Lookup([]byte(w))
+			}
+			i += numWords
+		}
+	})
+	b.Run("VedhavyasCuckoo", func(b *testing.B) {
+		f := cuckooVed.NewFilter(uint32(numWords))
+		for _, w := range words[:numWords] {
+			f.Insert([]byte(w))
+		}
+		b.ResetTimer()
+		for i := 0; i < b.N; {
+			for _, w := range mixedWords[:numWords] {
 				f.Lookup([]byte(w))
 			}
 			i += numWords
